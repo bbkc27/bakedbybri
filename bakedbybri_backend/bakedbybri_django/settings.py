0f612a448a3b7dc277da38162748a27893bc20c9
bbkc27 <briana.j.casey@gmail.com>
2023-01-11 21:23:45 -0500

    Back end skeleton

--- bakedbybri_backend/bakedbybri_django/settings.py
+++ bakedbybri_backend/bakedbybri_django/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'bakedbybri_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bakedbybri',
+        'USER': 'bakedbybriuser',
+        'PASSWORD': 'bakedbybri',
+        'HOST': 'localhost'
     }
 }
 
