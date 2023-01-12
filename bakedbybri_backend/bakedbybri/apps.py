3a27ab2a8fb27f1282fc2921b3196c9787a9d1ac
bbkc27 <briana.j.casey@gmail.com>
2023-01-11 21:15:29 -0500

    Backend skeleton

---
+++ bakedbybri_backend/bakedbybri/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BakedbybriConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bakedbybri'
---
+++ bakedbybri_backend/settings.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE bakedbybri;
+CREATE USER bakedbybriuser WITH PASSWORD 'bakedbybri';
+GRANT ALL PRIVILEGES ON DATABASE bakedbybri TO bakedbybriuser;
