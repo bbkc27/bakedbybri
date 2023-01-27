d47d4891bba83d1c38472459b7ca6ba57a087a56
bbkc27 <briana.j.casey@gmail.com>
2023-01-27 16:43:45 -0500

    Key update

--- bakedbybri_backend/bakedbybri_django/settings.py
+++ bakedbybri_backend/bakedbybri_django/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.environ.get('MODE') == 'dev' else False
+DEBUG = True if os.environ['MODE'] == 'dev' else False
 
 ALLOWED_HOSTS = ['*']
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bakedbybri',
+    'django_extensions',
     'rest_framework',
 ]
 
