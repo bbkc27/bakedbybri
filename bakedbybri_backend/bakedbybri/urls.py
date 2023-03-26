d97687a8910b735816931ba022bff422cc79109f
bbkc27 <briana.j.casey@gmail.com>
2023-03-26 11:45:12 -0400

    Views and URLs

---
+++ bakedbybri_backend/bakedbybri/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    
+    path('signup/', views.CreateUser.as_view()),
+
+    path('posts/', views.PostList.as_view()),
+    path('posts/<int:pk>', views.PostDetail.as_view(), name='post_detail'),
+    path('ingredients/', views.IngredientList.as_view()),
+    path('ingredients/<int:pk', views.IngredientDetail.as_view())
+
+]
--- bakedbybri_backend/bakedbybri/views.py
+++ bakedbybri_backend/bakedbybri/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from .models import Ingredients, Post
+from .serializers import PostSerializer, IngredientSerializer, UserSerializer, MyTokenObtainPairSerializer
 
 # Create your views here.
+
+class CreateUser(generics.CreateAPIView):
+    model = get_user_model
+    permissions_classes = [permissions.AllowAny]
+    serializer_class = UserSerializer
+
+class IngredientList(generics.ListCreateAPIView):
+    serializer_class = IngredientSerializer
+    queryset = Ingredients.objects.all()
+    permissions_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class IngredientDetail(generics.RetrieveUpdateDestroyAPIView):
+   serializer_class = IngredientSerializer
+   queryset = Ingredients.objects.all()
+   permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class PostList(generics.ListCreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    permissions_classes = [permissions.AllowAny]
+
+    def post(self, request, *args, **kwargs):
+      print(request, request.user)
+      request.data['user'] = request.user.username
+      return self.create(request, *args, **kwargs)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    permissions_classes = [permissions.AllowAny]
+
+    def put(self, request, *args, **kwargs):
+      print(request)
+      return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+      return self.destroy(request, *args, **kwargs)
+
+
+
